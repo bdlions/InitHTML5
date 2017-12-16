c21a12dc9a4f95fde76fe631514dec232d64ce0c
nazmulhasan <bdlions@gmail.com>
2017-12-16 12:32:43 +0600

    js and js map files are deleted

--- app/webservice/Packet.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Packet = (function () {
-    function Packet() {
-    }
-    return Packet;
-}());
-exports.Packet = Packet;
-//# sourceMappingURL=Packet.js.map
--- app/webservice/PacketHeader.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var PacketHeader = (function () {
-    function PacketHeader() {
-    }
-    return PacketHeader;
-}());
-exports.PacketHeader = PacketHeader;
-//# sourceMappingURL=PacketHeader.js.map
